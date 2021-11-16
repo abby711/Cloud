caa714ac61c4101a45bed13133033244ea0fa3d6
Abirami <65455645+abby711@users.noreply.github.com>
2021-11-16 22:29:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+
+
+from flask import Flask,render_template,request, url_for, redirect
+from flask_pymongo import PyMongo
+app = Flask(__name__)
+
+
+
+
+app.config['MONGO_URI'] = 'mongodb+srv://abby:abby@freecluster.lvlvs.mongodb.net/rsa?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+
+datas = mongo.db.rsa
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+@app.route('/add', methods=['POST'])
+def addData():
+    p = request.form.get('bid')
+    q = request.form.get('bname')
+    r = request.form.get('aname')
+    s = request.form.get('sub')
+    
+    datas.insert_one({'BookId' : p, 'BookName' : q,'AuthorName':r,'Subject':s})
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+   app.run()
+   
+
+
